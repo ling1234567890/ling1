4302db0dbb0636bf37eb6885861bbef80b6d2814
ling1234567890 <274179109@qq.com>
2017-02-05 13:54:02 +0800

    详情页

---
+++ src/js/detail-page-jq.js
@@ -0,0 +1,26 @@
+$(function() {
+	
+	//滚动条事件 顶部悬浮
+	$(function() {
+		//获取要定位元素距离浏览器顶部的距离
+		var navH = $("#xuanting").offset().top;
+		$(window).scroll(function() {
+			//获取滚动条的滑动距离
+			var scroH = $(this).scrollTop();
+			//滚动条的滑动距离大于等于定位元素距离浏览器顶部的距离，就固定，反之就不固定
+			if (scroH >= navH) {
+				$("#xuanting").css({
+					"position": "fixed",
+					"top": 0
+				});
+				$("#xuanting").addClass('ss');
+			} else{
+				$("#xuanting").css({
+					"position": "static"
+				});
+				$("#xuanting").removeClass('ss');
+			}
+		})
+	})
+
+});
